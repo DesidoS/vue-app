22f45034bcdabbd7b2f5005e944d43e8e321ad3f
Oleksii Kravchenko <djdesidos@gmail.com>
2023-03-14 15:56:06 +0200

    add

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <div class="content">
+      <AppHeader />
+      <router-view></router-view>
+    </div>
+    <AppFooter />
+  </div>
+</template>
+
+<script>
+import AppHeader from "./components/header";
+import AppFooter from "./components/Footer";
+
+export default {
+  name: "App",
+  components: {
+    AppFooter,
+    AppHeader,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+#app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  font-family: Montserrat, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+.content {
+  flex-grow: 1;
+}
+</style>
---
+++ src/pages/Homepage.vue
@@ -0,0 +1,74 @@
+<template>
+  <main>
+    <SectionWithHeaderSpacer>
+      <Container>
+        <ApartmentsFilterForm class="apartments-filter" @submit="filter" />
+      </Container>
+      <Container>
+        <p v-if="!filteredApartments.length">Ничего не найдено</p>
+        <ApartmentsList v-else :items="filteredApartments" />
+      </Container>
+    </SectionWithHeaderSpacer>
+  </main>
+</template>
+
+<script>
+import ApartmentsFilterForm from "../components/apartment/ApartmentsFilterForm.vue";
+import ApartmentsList from "../components/apartment/ApartmentsList.vue";
+import apartments from "../components/apartment/apartments.js";
+import Container from "../components/shared/Container.vue";
+import SectionWithHeaderSpacer from "../components/shared/SectionWithHeaderSpacer";
+
+export default {
+  name: "App",
+  components: {
+    ApartmentsList,
+    ApartmentsFilterForm,
+    Container,
+    SectionWithHeaderSpacer,
+  },
+
+  data() {
+    return {
+      apartments,
+      text: "",
+      filters: {
+        city: "",
+        price: 0,
+      },
+    };
+  },
+
+  computed: {
+    filteredApartments() {
+      return this.filterByCityName(this.filterByPrice(this.apartments));
+    },
+  },
+  methods: {
+    filter({ city, price }) {
+      this.filters.city = city;
+      this.filters.price = price;
+    },
+    filterByCityName(apartments) {
+      if (!this.filters.city) return apartments;
+
+      return apartments.filter((apartments) => {
+        return apartments.location.city === this.filters.city;
+      });
+    },
+    filterByPrice(apartments) {
+      if (!this.filters.price) return apartments;
+
+      return apartments.filter((apartments) => {
+        return apartments.price >= this.filters.price;
+      });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.apartments-filter {
+  margin-bottom: 40px;
+}
+</style>
